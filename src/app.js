ef80553f91f98fc1a73f0a68a301a2ad401e859b
RaullamaMindata <raul.lama@mindata.es>
2025-05-06 10:44:19 +0200

    push railway

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import { PORT } from './config.js'
 
-// Importar rutas modularizadassa
+// Importar rutas modularizadas
 import usuarisRoutes from './routes/usuaris.js'
 import plantasRoutes from './routes/plantas.js'
 import itemsRoutes from './routes/items.js'
